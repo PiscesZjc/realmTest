38adba1d2c979253ccea68402980171e4f137071
zjc <zhaojiacong@hq51.com.cn>
2017-03-20 15:19:48 +0800

    realm

---
+++ ZJCrealm/ZJCrealm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJCrealm
+//
+//  Created by zjc on 2017/3/20.
+//  Copyright © 2017年 zjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZJCrealm/ZJCrealm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZJCrealm
+//
+//  Created by zjc on 2017/3/20.
+//  Copyright © 2017年 zjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
